f33fcbe050504d581934282fa2a34a82a659f087
Ahmed Sabbour <sabbour@outlook.com>
2015-03-10 20:58:23 +0200

    More edits

--- postgresql/CustomScripts/configure-postgresql.sh
+++ postgresql/CustomScripts/configure-postgresql.sh
@@ -7,22 +7,16 @@ logger "Setting ownership"
 chown postgres:postgres /var/lib/pgsql
 
 if [ $(hostname) == $1 ]; then
-logger "Initializing PostgreSQL on Primary node $1"
-postgresql-setup initdb
+  logger "Initializing PostgreSQL on Primary node $1"
+  postgresql-setup initdb
+  logger "Enabling listening on all IPs"
+  sed -i.bak "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/pgsql/data/postgresql.conf
 fi
-
-logger "Enabling listening on all IPs"
-sed -i.bak "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/pgsql/data/postgresql.conf
 
 logger "Unmount file system from Primary Node and revert it to be Secondary"
 if [ $(hostname) == $1 ]; then
-    umount /var/lib/pgsql/data
+    umount /var/lib/pgsql
     drbdadm secondary r0
-fi
-
-logger "Deleting any content data directory from Secondary Node as this will be synced via DRBD"
-if [ $(hostname) == $2 ]; then
-    rm -Rf /var/lib/pgsql/data/*
 fi
 
 logger "Disabling automatic startup of PostgreSQL as it will be controlled by Pacemaker"
